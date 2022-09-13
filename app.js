b5a85181253bb027a0d8a5327b2dde04b6e7f5fd
thomasdl92 <thomasdelaet@hotmail.com>
2022-09-13 22:25:27 +0200

    changes

--- app.js
+++ app.js
@@ -57,26 +57,6 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.CLIENT_ID,
-//       clientSecret: process.env.CLIENT_SECRET,
-//       callbackURL: "http://localhost:3000/auth/google/secrets",
-//       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-//     },
-//     (accessToken, refreshToken, profile, cb) => {
-//       console.log(profile);
-
-//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//         return cb(err, user);
-//       });
-//     }
-//   )
-// );
-
-//BETTER USE THE CODE BELOW
-
 //GOOGLE
 passport.use(
   new GoogleStrategy(
@@ -139,6 +119,7 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+//GOOGLE
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
@@ -153,6 +134,7 @@ app.get(
   }
 );
 
+//FACEBOOK
 app.get(
   "/auth/facebook",
   passport.authenticate("facebook", { scope: ["email"] })
